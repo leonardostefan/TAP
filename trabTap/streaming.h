d47d536b7342e17c17a53317dbf408f82c1ee3b2
Cesar Camillo <cesaraugustoalvescamillo@hotmail.com>
2017-11-09 15:54:00 -0200

    Update streaming.h

--- trabTap/streaming.h
+++ trabTap/streaming.h
@@ -7,25 +7,25 @@
 
 class Streaming  //Singleton patter
 {
-
- private:	
-  Streaming();
-  static Streaming instance=NULL;
   
- public:
-  Streaming instanceStraming(string, string); //instancia uma
-  string password; 	//Senha
-  string userLogin;	//Login 
-  int usetType; 		//Plano
-  Diretorio directory; //Diretorio de musicas
-  
-  
+  private:	
+    Streaming();
+    static Streaming instance=NULL;
+    
+  public:
+    Streaming instanceStraming(string, string); //instancia uma
+    char password[STRING_MAX]; 	//Senha
+    char userLogin[STRING_MAX];	//Login 
+    int usetType; 		//Plano
+    Diretorio directory; //Diretorio de musicas
+    
+    
 };
 
 class Diretorio //Composite Visito
 {
-  Playlist playlists[ALBUM_MAX]={NULL};
-  char estiloPesquisa[STRING_MAX];
+    Playlist playlists[ALBUM_MAX]={NULL};
+    char estiloPesquisa[STRING_MAX];
 };
 
 class Playlist
@@ -59,30 +59,51 @@ class Visitor
 class Todos : Visitor
 {
   void visitar(Diretorio diretorio) override{
-    for (int i=0; diretorio.playlists[i]!=NULL; i++){
-      printf ("P: %s\n", diretorio.playlists[i].nome);
-      for (int j=0; diretorio.playlists[i].albuns[j]!=NULL; j++){
-	printf("A: %s\n", diretorio.playlists[i].albuns[j].nome);
-	for (int k=0; k < 5; k++)
-	  printf ("M: %s\n", diretorio.playlists[i].albuns[j].musicas[k].nome); 
-      }
+      for (int i=0; diretorio.playlists[i]!=NULL; i++){
+	  printf ("P: %s\n", diretorio.playlists[i].nome);
+	    for (int j=0; diretorio.playlists[i].albuns[j]!=NULL; j++){
+	      printf("A: %s\n", diretorio.playlists[i].albuns[j].nome);
+		for (int k=0; k < 5; k++)
+		  printf ("M: %s\n", diretorio.playlists[i].albuns[j].musicas[k].nome); 
+	    }
+	}
     }
-  }
 };
 
 class Estilos : Visitor
 {
-  void visitar(Diretorio diretorio){
-    for (int i=0; diretorio.playlists[i]!=NULL; i++)
-      for (int j=0; diretorio.playlists[i].albuns[j]!=NULL;j++)
-	for (int k=0; k < 5; k++)
-	  if (strcmp(diretorio.playlists[i].albuns[j].musicas[k].nome,diretorio.estiloPesquisa))
-	    printf("%s\n", diretorio.playlists[i].albuns[j].musicas[k].nome);
-  }
-
+    void visitar(Diretorio diretorio){
+      for (int i=0; diretorio.playlists[i]!=NULL; i++)
+	for (int j=0; diretorio.playlists[i].albuns[j]!=NULL;j++)
+	  for (int k=0; k < 5; k++)
+	    if (strcmp(diretorio.playlists[i].albuns[j].musicas[k].nome,diretorio.estiloPesquisa))
+	      printf("%s\n", diretorio.playlists[i].albuns[j].musicas[k].nome);
+    }
+    
 };
 
 class Tempo : Visitor
 {
-  
+  void visitar(Diretorio diretorio){
+    for (int i=0; diretorio.playlists[i]!=NULL; i++)
+      if (strcmp(diretorio.playlists[i].estilo,diretorio.estiloPesquisa))
+	{
+	  printf("Playlist %s com o estilo %s\n", diretorio.playlists[i].nome, diretorio.playlists[i].estilo);
+	  for (int j=0; diretorio.playlists[i].albuns[j]!=NULL;j++)
+	    if (strcmp(diretorio.playlists[i].albuns[j].estilo, diretorio.estiloPesquisa))
+	      for (int k=0; diretorio.playlists[i].albuns[j].musicas[k]!=NULL;k++)
+		if (diretorio.playlists[i].albuns[j].musicas[k].duracao > 180)
+		  printf("Musica %s com o estilo %s", diretorio.playlists[i].albuns[j].musicas[k].nome, diretorio.playlists[i].albuns[j].musicas[k].estilo)
+		    }
+    
+      else if (strcmp(diretorio.playlists[i].estiloSec,diretorio.estiloPesquisa))
+	{
+	  printf ("Playlist %s com o estilo secundário %s\n", diretorio.playlists[i].nome, diretorio.playlists[i].estiloSec);
+	  for (int j=0; diretorio.playlists[i].albuns[j]!=NULL;j++)
+	    if (strcmp(diretorio.playlists[i].albuns[j].estilo, diretorio.estiloPesquisa))
+	      for (int k=0; diretorio.playlists[i].albuns[j].musicas[k]!=NULL;k++)
+		if (diretorio.playlists[i].albuns[j].musicas[k].duracao > 180)
+		  printf("Musica %s com o estilo %s", diretorio.playlists[i].albuns[j].musicas[k].nome, diretorio.playlists[i].albuns[j].musicas[k].estilo)
+		    }
+  }
 }
