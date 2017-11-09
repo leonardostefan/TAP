2be25a6f75e77051dc17ca0e6ccdf1b9e6b6ad13
Cesar Camillo <cesaraugustoalvescamillo@hotmail.com>
2017-11-09 17:04:34 -0200

    Add files via upload

---
+++ trabTap/streaming.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+class Streaming
+{
+    private Streaming();
+    private static Streaming instance=NULL;
+    
+    public Streaming instanciaStreaming(string, string); //instancia uma
+    public String senha; 	
+    public String login;	 
+    public int tipoUsuario; 		//Plano
+    public Diretorio diretorio; //Diretorio de musicas
+
+}
+
+class Diretorio
+{
+    ArrayList<Playlist> playlists;
+    private String estiloPesquisa;
+
+    public void addPlaylist (Playlist lista)
+    {
+	this.playlists.add(lista);
+    }
+}
+
+class Playlist extends Diretorio
+{
+    ArrayList<Album> albuns;
+    String nome, estilo, estiloSec;
+
+    public void criaPlaylsit (String name, String style, String style2)
+    {
+	this.nome = nome;
+	this.estilo = style;
+	this.estiloSec = style2;
+    }
+
+    public void addAlbum (Album musicAlbum)
+    {
+	this.albuns.add(musicAlbum);
+    }
+}
+
+class Album extends Diretorio
+{
+    ArrayList<Musica> musicas;
+    String nome, banda, estilo;
+
+    public void criaAlbum (String name, String band, String style)
+    {
+	this.nome = name;
+	this.banda = band;
+	this.estilo = style;
+    }
+
+    public void addMusica (Musica music)
+    {
+	this.musicas.dd(music);
+    }
+}
+
+class Musica
+{
+    int duracao; // em segundos
+    String nome, estilo;
+
+    public void criaMusica (int dur, String name, String style)
+    {
+	this.duracao = dur;
+	this.nome = name;
+	this.estilo = style;
+    }
+
+}
